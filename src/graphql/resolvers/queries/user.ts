68f22321414edc0364dc1ad1e3702f2e26296579
Ostad <SOstad@kajimausa.com>
2020-06-17 15:29:24 -0400

    feat: query userGetByUserName added feat: query userGetByName added feat: query userGetByNameApproxMatch added

--- src/graphql/resolvers/queries/user.ts
+++ src/graphql/resolvers/queries/user.ts
@@ -1,17 +1,41 @@
 import * as directory from "ldap-directory-manager";
 import { transformData } from "../../../helpers/utils";
 import { writeLog } from "fast-node-logger";
-import { GraphqlContext } from "../../../typings/general/context";
+import { QueryResolvers } from "../../../typings/generated/types";
 
-export async function userGetAll(
-  _: any,
-  { criteria }: any,
-  { connectionInfo }: GraphqlContext,
-) {
+export const userGetAll: QueryResolvers["userGetAll"] = async (
+  _,
+  { criteria },
+  { connectionInfo },
+) => {
   writeLog(`userGetAll()`, { level: "trace" });
   const client = new directory.Client(connectionInfo);
   try {
-    const data = await directory.userGetAll(criteria, {
+    const data = await directory.userGetAll({
+      criteria: criteria ?? undefined,
+      configs: {
+        client,
+        attributes: ["*"],
+      },
+    });
+
+    const transferredData = data.map((el) => transformData(el));
+
+    return transferredData;
+  } finally {
+    client.unbind();
+  }
+};
+
+export const userGetByUserName: QueryResolvers["userGetByUserName"] = async (
+  _,
+  { userName },
+  { connectionInfo },
+) => {
+  writeLog(`userGetAll()`, { level: "trace" });
+  const client = new directory.Client(connectionInfo);
+  try {
+    const data = await directory.userGetByUserName(userName, {
       client,
       attributes: ["*"],
     });
@@ -22,4 +46,46 @@ export async function userGetAll(
   } finally {
     client.unbind();
   }
-}
+};
+
+export const userGetByName: QueryResolvers["userGetByName"] = async (
+  _,
+  { cn },
+  { connectionInfo },
+) => {
+  writeLog(`userGetByName()`, { level: "trace" });
+  const client = new directory.Client(connectionInfo);
+  try {
+    const data = await directory.userGetByName(cn, {
+      client,
+      attributes: ["*"],
+    });
+
+    const transferredData = data.map((el) => transformData(el));
+
+    return transferredData;
+  } finally {
+    client.unbind();
+  }
+};
+
+export const userGetByNameApproxMatch: QueryResolvers["userGetByNameApproxMatch"] = async (
+  _,
+  { cn },
+  { connectionInfo },
+) => {
+  writeLog(`userGetByNameApproxMatch()`, { level: "trace" });
+  const client = new directory.Client(connectionInfo);
+  try {
+    const data = await directory.userGetByNameApproxMatch(cn, {
+      client,
+      attributes: ["*"],
+    });
+
+    const transferredData = data.map((el) => transformData(el));
+
+    return transferredData;
+  } finally {
+    client.unbind();
+  }
+};
